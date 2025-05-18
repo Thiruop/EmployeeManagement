95625cc690e50c7baddece67793fbb5e7b38c5ec
Thiru <114560047+Thiruop@users.noreply.github.com>
2025-05-18 21:01:16 +0530

    Delete Routes directory

--- Routes/Employee.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { PostEmployee, GetEmployee, UpdateEmployee, DeleteEmployee } from '../Controller/EmployeeDetail.js';
-
-router.post('/employee', PostEmployee);
-router.get('/employees', GetEmployee);
-router.put('/employee/:id', UpdateEmployee);
-router.delete('/employee/:id', DeleteEmployee);
-export default router;
